6bccc65e656dcd0167b944c53b286f9bf1e42b21
Bruno Baffini <bruno.baffini@quintoandar.com.br>
2023-05-01 17:06:28 -0300

    Setting up project

---
+++ Server/src/Part.java
@@ -0,0 +1,43 @@
+import java.util.List;
+
+public class Part {
+    int id;
+    String name;
+    String description;
+    List<Part> subcomponents;
+
+    public Part(int id, String name, String description, List<Part> subcomponents) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.subcomponents = subcomponents;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<Part> getSubcomponents() {
+        return subcomponents;
+    }
+
+    public void setSubcomponents(List<Part> subcomponents) {
+        this.subcomponents = subcomponents;
+    }
+}
